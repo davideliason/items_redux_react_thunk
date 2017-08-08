f2029fab175362b9cedeec86a30e3d6d9a3bc471
davideliason <davethemaker@gmail.com>
2017-08-08 16:26:27 -0700

    successfully implement ItemList which uses hard-coded array of object data to UI display

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import ItemList from './ItemList.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p>hello world</p>
+        <ItemList />
       </div>
     );
   }
