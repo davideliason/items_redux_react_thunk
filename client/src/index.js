05ec357d929cf9f2c229b410ec40fd2a3ccf6001
davideliason <davethemaker@gmail.com>
2017-08-08 17:26:34 -0700

    add redux store, add Provider for app state, renders correctly

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,15 @@ import App from './components/App.js';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers/index.js'
+import rootReducer from './reducers/index.js';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+function configureStore(initialState){
+	return createStore(rootReducer,initialState,applyMiddleware(thunk));
+}
+const store = configureStore();
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
